9a102eef44789a9540f5366506e7cfe5ab8a7af8
bishnuNavsoft <bishnu.behera@navsoft.in>
2025-02-03 17:15:03 +0530

    do image multiple

--- models/Item.js
+++ models/Item.js
@@ -4,7 +4,7 @@ const ItemSchema = new mongoose.Schema({
     name: { type: String, required: true },  
     category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },  
     price: { type: Number, required: true },  
-    image: { type: String, required: false },
+    image: [{ type: String, required: false }],
 });  
 
 module.exports = mongoose.model('Item', ItemSchema);
